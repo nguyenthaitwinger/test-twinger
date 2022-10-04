359ca78c13675ee5072c55ec1775ece49aa72809
Nguyen Hong Thai <nguyenthai9cc@gmail.com>
2022-10-04 09:37:34 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
